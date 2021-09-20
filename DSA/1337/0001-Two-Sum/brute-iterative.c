bc10f359aefcaba6845873113b6891d1d0289d37
veganaiZe <veganaize@users.noreply.github.com>
2021-09-19 21:13:54 -0700

    Create brute-iterative.c

---
+++ DSA/1337/0001-Two-Sum/brute-iterative.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    //nums = [2,7,11,15], target = 9  /* [0, 1] */
+    //nums = [3,2,4], target = 6     /* [1,2] */
+    //nums = [3,3], target = 6      /* [0,1] */
+
+    return 0;
+}
+
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int* twoSum(int* nums, int numsSize, int target, int* returnSize){
+
+}
